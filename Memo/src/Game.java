e6dda45aa2cc21f6d6215d3fee8b38f2a76603be
Pawel Bedra <s21963@pjwstk.edu.pl>
2022-06-19 11:53:23 +0200

    Memo - można zaczynać

---
+++ Memo/src/Game.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Game {
+
+    public static void main(String[] args) {
+
+        //Tworzę ramkę JFrame
+        JFrame frame = new JFrame();
+        frame.setSize(1200, 800);
+        frame.setTitle("Tytuł ramki");
+        frame.setVisible(true);
+
+
+    }
+}
